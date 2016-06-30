3fe539d482a277721a868cf712780f40cc2591d8
Chris Myers <cmyers78@gmail.com>
2016-06-29 21:38:12 -0600

    App is completed....want to work on some spacing issues.

--- WeatherWasp/WeatherWasp/WeatherInfoViewController.swift
+++ WeatherWasp/WeatherWasp/WeatherInfoViewController.swift
@@ -18,12 +18,18 @@ class WeatherInfoViewController: UIViewController, WeatherAPIDelegate {
     
     @IBOutlet weak var temperatureLabel: UILabel!
     
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var summaryLabel: UILabel!
+    
+    @IBOutlet weak var precipLabel: UILabel!
+    
+    @IBOutlet weak var humidLabel: UILabel!
+    
+    
     var passedCity = City()
     
     let apiController = APIController()
-    
-    
-    
     
     
     override func viewDidAppear(animated: Bool) {
@@ -48,6 +54,56 @@ class WeatherInfoViewController: UIViewController, WeatherAPIDelegate {
         
         print("the weather has passed")
         self.temperatureLabel.text = String(Int(weather.temperature)) + "°"
+        self.precipLabel.text = String(weather.precipProbability) + "%"
+        
+        let humid = (weather.humidity) * 100
+        self.humidLabel.text = String(humid) + "%"
+        
+        if weather.icon == "clear-day" {
+            self.imageView.image = UIImage(named: "clear-day")
+            self.summaryLabel.text = "Clear Day"
+        }
+        
+        else if weather.icon == "clear-night" {
+            self.imageView.image = UIImage(named: "clear-night")
+            self.summaryLabel.text = "Clear Night"
+        }
+        
+        else if weather.icon == "cloudy" {
+            self.imageView.image = UIImage(named: "Cloudy")
+            self.summaryLabel.text = "Cloudy"
+        }
+        
+        else if weather.icon == "partly-cloudy-day" {
+            self.imageView.image = UIImage(named: "partly-cloudy-day")
+            self.summaryLabel.text = "Daytime. Partly Cloudy"
+        }
+        
+        else if weather.icon == "partly-cloudy-night" {
+            self.imageView.image = UIImage(named: "partly-cloudy-night")
+            self.summaryLabel.text = "Nighttime. Partly Cloudy"
+        }
+        
+        else if weather.icon == "rain" {
+            self.imageView.image = UIImage(named: "rain")
+            self.summaryLabel.text = "Rain"
+        }
+        
+        else if weather.icon == "snow" {
+            self.imageView.image = UIImage(named: "snow")
+            self.summaryLabel.text = "Snow"
+        }
+        
+        else if weather.icon == "wind" {
+            self.imageView.image = UIImage(named: "wind")
+            self.summaryLabel.text = "Windy"
+        }
+        
+        else {
+            self.imageView.image = UIImage(named: "clear-day")
+            self.summaryLabel.text = "Unavailable"
+        }
+        
         
     }
 
